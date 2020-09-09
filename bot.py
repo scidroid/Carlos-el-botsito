be0b782c5eed5fa533b798c70280d5de97ba156e
SciDroid <scidroidgames@gmail.com>
2020-09-08 19:42:02 -0500

    Sistema basico de votacion agregado

--- bot.py
+++ bot.py
@@ -49,15 +49,64 @@ async def event_message(ctx):
         await ctx.channel.send("Hey, ya sigues a Freddy en todas sus redes? no, no lo hiciste, pues te llevas un puño en tu boca")
         time.sleep(1800) 
 
-Frases = ['Te quiero mucho', 'La teoria de la gravedad de Einstein', 'Yo no soy senior, ni mid, ni junior yo soy ingeniero', '¿Todo bien en casa?', 'Mis mods son simps', 'Coño *Inserte persona del chat*']
 
 @bot.command(name="frase")
 async def Frase(ctx):
-    await ctx.channel.send(random.choice(Frases) + ' -Freddy 2020.')
+    await ctx.channel.send(random.choice('Te quiero mucho', 'La teoria de la gravedad de Einstein', 'Yo no soy senior, ni mid, ni junior yo soy ingeniero', '¿Todo bien en casa?', 'Mis mods son simps', 'Coño *Inserte persona del chat*') + ' -Freddy 2020.')
 
 @bot.command(name='Source')
 async def Source(ctx):
     await ctx.channel.send('Mira como funciono por dentro, https://github.com/scidroid/carlos-el-botsito')
+    
+uno= 0
+
+dos= 0
+
+tres= 0
+
+cuatro= 0
+
+cinco= 0
+
+@bot.event
+async def event_message(ctx):
+    if ctx.author.name.lower() == os.environ['BOT_NICK'].lower() or ctx.author.name.lower() == 'Nightbot'.lower():
+        return
+
+    await bot.handle_commands(ctx)
+
+    # await ctx.channel.send(ctx.content)
+
+    if ctx.content.lower() == 1:
+        uno += 1
+
+    if ctx.content.lower() == 2:
+        dos += 1
+
+    if ctx.content.lower() == 3:
+        tres += 1
+
+    if ctx.content.lower() == 4:
+        cuatro += 1
+
+    if ctx.content.lower() == 5:
+        cinco += 1
+
+
+@bot.command(name="resultados")
+async def resultados(ctx):
+    await ctx.channel.send(f"1 tiene {uno} votos, 2 tiene {dos} votos, 3 tiene {tres} votos, 4 tiene {cuatro} votos, 5 tiene {cinco} votos.")
+
+
+
+@bot.command(name="resetvots")
+async def resetvots(ctx):
+    uno = 0
+    dos = 0
+    tres = 0
+    cuatro = 0
+    cinco = 0
+
 
 if __name__ == "__main__":
     bot.run()
